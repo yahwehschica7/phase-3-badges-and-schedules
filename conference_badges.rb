387b36f4ab539af8150883203bed6ebca54f6f20
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-06 15:26:09 -0400

    commit tree 90a7abd8aeff56fb6f2dfa1775e5c7bf7004bcdc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+   names.map { |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room_number|
+      "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    end
+  end
+
+def printer(names)
+
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end 
